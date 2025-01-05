4c0d95f42bb0c53c76f02312e327969345ae4cda
sameersultan41 <sameer@linkedunion.com>
2025-01-05 14:22:48 +0500

    add testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -15,16 +15,23 @@ const Testimonials = () => {
 		{
 			id: 1,
 			image: Image4,
-			title: "Kerren Ortlepp",
-			subtitle: "Founder @ Entrostat",
+			title: "Sharjeel UR Rehman",
+			subtitle: "Frontend Engineer @ Maksof ",
 			comment: "Greg worked at Entrostat as a full-stack developer. He was excellent and had a real eye for design. If there was ever a time where we had a complicated frontend or needed to know what the best CSS approach was for something - Greg was our goto man!",
 		},
 		{
 			id: 2,
 			image: Image3,
-			title: "Thapelo Kekana",
-			subtitle: "Frontend Developer @ FNB",
+			title: "Zain Imam",
+			subtitle: "SQA Engineer @ Avialdo ",
 			comment: "I enjoy working with the theme and learn so much. You guys make the process fun and interesting. Good luck! 👍",
+		},
+		{
+			id: 3,
+			image: Image4,
+			title: "Faizan Ahmed",
+			subtitle: "Backend Engineer @ Avialdo ",
+			comment: "I had the privilege of working with Sameer, a talented Junior Frontend Engineer who combines technical expertise with a keen eye for design. He consistently delivers high-quality, user-friendly web applications, demonstrates proactive learning, and fosters a positive team environment. I highly recommend Sameer for any team seeking a skilled and motivated frontend developer.",
 		},
 	];
 
